92181fa631e49941b054fcde0893d581b0e89fad
Theophilus Asamoah Gyapong <jhapson34@gmail.com>
2025-06-26 11:48:36 +0000

    app colors for theme

---
+++ fer_app/lib/themes/appColor.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color textColor = const Color.fromARGB(255, 170, 170, 172);
+  static Color textColorDark = const Color(0xffffffff);
+
+  static Color bodyColor = const Color.fromARGB(255, 255, 255, 255);
+  static Color bodyColorDark = const Color.fromARGB(255, 13, 13, 14);
+
+  static Color buttonBackgroundColor = const Color.fromARGB(255, 250, 249, 249);
+  static Color buttonBackgroundColorDark =
+      const Color.fromARGB(255, 17, 17, 17);
+}
